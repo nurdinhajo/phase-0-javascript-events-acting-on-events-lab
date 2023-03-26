5b5ed29854f57396668c5bd7f4c14e700a612bfe
nuru <nurudiin222@gmail.com>
2023-03-26 13:51:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Your code here
+
+//To the left
+
+function moveDodgerLeft() {
+ var leftNumbers = dodger.style.left.replace('px', '')
+ var left = parseInt(leftNumbers, 10)
+
+ if (left > 0) {
+   dodger.style.left = `${left - 1}px`
+ }
+}
+
+
+//To the right
+
+function moveDodgerRight() {
+ var leftNumbers = dodger.style.left.replace('px', '');
+ var left = parseInt(leftNumbers, 10);
+ if (left < 360) { // assuming 400px container width and 40px dodger width
+   dodger.style.left = `${left + 1}px`;
+ }
+}
